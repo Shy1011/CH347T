ad62291c58ace6770b8a5d9bc69c34cecf19d250
RanShuai 2633609R <150208066+Shy1011@users.noreply.github.com>
2024-07-14 23:30:37 +0800

    done

--- ch347/debug.py
+++ ch347/debug.py
@@ -22,55 +22,72 @@ print(dev.get_version())
 
 # """Test if USB is connected"""
 # # dev.set_device_notify('HID\\VID_1A86&PID_55DC&MI_01#7&5D28A11&0&0000#')
-#
-# spiCon = SPIConfig()
-# spiCon.Mode = 0
-# spiCon.Clock = 3
-# spiCon.ByteOrder = 1
-# spiCon.SPIWriteReadInterval = 1
-# spiCon.SPIOutDefaultData = 0x0000
-# spiCon.ChipSelect = 0X80
-# spiCon.CS1Polarity = 0
-# spiCon.CS1Polarity = 0
-# spiCon.IsAutoDeativeCS = 1
-# spiCon.ActiveDelay = 2
-# spiCon.ActiveDelay = 2
-#
-# """SPI init"""
-# print("SPI init")
-# print(dev.spi_init(spiCon))
-#
-#
-# """SPI Config"""
-# print("SPI Config")
-# print(dev.spi_get_cfg())
-#
-# """SPI Write"""
-# print("SPI Write")
-# print(dev.spi_write(0x80, bytes([0x80,0x88])))
-# # 如果这里出现问题就可能是因为CS pin没选对 或者chip_select写的不对
-# # 另外还有一些延时
-# # 另外这里还要测试一下这是发16 还是8 位,以及18 / 8 位发送设置怎么选
-# # device_index = 0 是什么意思
-#
-# """SPI read"""
-# print("SPI Read")
-# print(dev.spi_read(0X80, bytes([0x00,0xFF]), 2))
-while(1) :
+while(1):
+    # spiCon = SPIConfig()
+    # spiCon.Mode = 0
+    # spiCon.Clock = 5
+    # spiCon.ByteOrder = 1
+    # spiCon.SPIWriteReadInterval = 1
+    # spiCon.SPIOutDefaultData = 0x0000
+    # spiCon.ChipSelect = 0X80
+    # spiCon.CS1Polarity = 0
+    # spiCon.CS1Polarity = 0
+    # spiCon.IsAutoDeativeCS = 1
+    # spiCon.ActiveDelay = 2
+    # spiCon.ActiveDelay = 2
 
-    """Speed Set"""
-    print("IIC Set")
-    print(dev.i2c_set(0))
+    spiCo = SPIConfig()
+    spiCo.Mode = 2
+    spiCo.Clock = 5
+    spiCo.ByteOrder = 1
+    spiCo.SPIWriteReadInterval = 1
+    spiCo.SPIOutDefaultData = 0x0000
+    spiCo.ChipSelect = 0X81
+    spiCo.CS1Polarity = 0
+    spiCo.CS1Polarity = 0
+    spiCo.IsAutoDeativeCS = 0
+    spiCo.ActiveDelay = 2
+    spiCo.ActiveDelay = 2
 
-    """IIC Stream"""
-    print("IIC Stream")
-    print(dev.stream_i2c(bytes([0xFF,0x00]), 2))
+    """SPI init"""
+    print("SPI init")
+    # print(dev.spi_init(spiCon))
 
-    # """GPIO Get"""
-    # print("GPIO Get")
-    # print(dev.CH347GPIO_Get())
 
-    time.sleep(0.2)
+    # """SPI Config"""
+    # print("SPI Config")
+    # print(dev.spi_get_cfg())
+
+    """SPI Write"""
+    # print("SPI Write")
+    # print(dev.spi_write(0x80, bytes([0x00,0xFF])))
+    # 如果这里出现问题就可能是因为CS pin没选对 或者chip_select写的不对
+    # 另外还有一些延时
+    # 另外这里还要测试一下这是发16 还是8 位,以及18 / 8 位发送设置怎么选
+    # device_index = 0 是什么意思
+    dev.spi_init(spiCo)
+    print(dev.spi_write(0x91, bytes([0x00, 0xFF])))
+    # dev.spi_change_cs(1)
+
+
+    # """SPI read"""
+    # print("SPI Read")
+    # print(dev.spi_read(0X80, bytes([0x00,0x00]), 4))
+
+    time.sleep(0.1)
+# """Speed Set"""
+# print("IIC Set")
+# print(dev.i2c_set(0))
+
+# """IIC Stream"""
+# print("IIC Stream")
+# print(dev.stream_i2c(bytes([0xFF,0x88]), 2))
+
+# """GPIO Get"""
+# print("GPIO Get")
+# print(dev.CH347GPIO_Get())
+
+
 
 
 """close device"""
